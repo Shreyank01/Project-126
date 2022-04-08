0019f75ab9df757562b37e47efa9eed1bcc5c4a8
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2022-04-08 17:02:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-song1_lg = "";
-song2_in = "";
 
 function preload() {
     song1_lg = loadSound("LetGo_lg.mp3");
@@ -17,10 +15,20 @@ function setup() {
     poseNet.on('pose' , gotPoses);
 }
 
+function modelLoaded() {
+    console.log("poseNet is initialized");
+}
+
+
 left_wristX = 0;
 left_wristY = 0;
 right_wristX = 0;
 right_wristY = 0;
+song1_lg = "";
+song2_in = "";
+live_song_name = "";
+score_leftWrist = "";
+
 
 function gotPoses(results) {
     if(results.length > 0) {
@@ -35,12 +43,36 @@ function gotPoses(results) {
         console.log(" Left wrist X = " + left_wristX + " Left wrist Y = " + left_wristY);
         console.log(" Right wrist X = " + right_wristX + " Right wrist Y = " + right_wristY);
 
-
+        score_leftWrist = results[0].pose.keypoints[9].score;
+        console.log( "Score of left wrist is " + score_leftWrist);
     }
 }
 
 
+
+
 function draw() {
     image(video ,0 ,0 ,600 ,500);
+    stroke("red");
+    fill("red");
+
+}
+
+function draw(){
+    image(video,0,0,600,530);
+
+    fill("red");
+    stroke("red");
+
+    live_song_name = song1_lg.isPlaying();
+    console.log(live_song_name);
+
+    if(score_leftWrist > 0.2){
+        circle(left_wristX,left_wristY,20);
+        song2_in.stop();
+        if(live_song_name == false){
+            song1_lg.play();
+        }
+    }
 }
 
